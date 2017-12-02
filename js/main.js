9bd37bae487efb2bc6ce3a9f9262254a8dab28e9
tiagocorreia <tiagoalmeida.correia@gmail.com>
2017-12-02 19:27:13 +0000

    Responsive, favicon, page loader

--- js/main.js
+++ js/main.js
@@ -1,10 +1,44 @@
 $("document").ready(function () {
 
+    $("body").removeClass("loading");
+    
     /////////////////////////////////////
     //CLOSE NAVIGATION
     $(".navigation__link").on("click", function () {
         $("#nav-toggle").click();
     });
 
-
+    /////////////////////////////////////
+    //CSS TRICKS SMOOTH SCROLL
+    $(".smooth").click(function (event) { 
+        console.log("clicked");
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
 });
